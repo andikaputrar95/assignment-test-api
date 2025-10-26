d98f267bdd80d6a778839516733eab9580e488a4
Andika <andika.putra@dxtr.asia>
2025-10-26 14:40:24 +0700

    edit

--- TC_07 - Invalid User.js
+++ TC_07 - Invalid User.js
@@ -6,22 +6,18 @@ try {
     res = {};
 }
 
-// Validasi status code
 pm.test("Status code is 401 or 403", () => {
     pm.expect([401, 403]).to.include(pm.response.code);
 });
 
-// Validasi struktur error
 pm.test("Response contains 'error' key", () => {
     pm.expect(res).to.have.property("error");
 });
 
-// Validasi pesan error
 pm.test("Error message is 'Invalid or inactive API key'", () => {
     pm.expect(res.error).to.eql("Invalid or inactive API key");
 });
 
-// Pastikan tidak ada data lain
 pm.test("Response only contains 'error' field", () => {
     pm.expect(Object.keys(res).length).to.eql(1);
 });
